89472030d317019a9849822b89611e42acd299a6
andersonbosa <andersonbosa0@gmail.com>
2023-08-16 18:56:14 -0300

    chore: refactor initialization and paths in moshell.sh
    - Refactored the initialization of _MOSHEL_DIR_BASE_PATH  for cross-shell compatibility.

--- moshell.sh/custom/index.sh
+++ moshell.sh/custom/index.sh
@@ -2,14 +2,12 @@ _moshell::custom::index() {
   echo '# This is a index file to import (and export to shell) all files wanted. For better organization it was separeted by username.'
 }
 
-BASE_PATH="$(dirname "$(realpath "$0")")"
-
 # NOTE: The order matters
-source "$BASE_PATH/andersonbosa/ai_prompts.sh"
-source "$BASE_PATH/andersonbosa/docker.sh"
-source "$BASE_PATH/andersonbosa/generators.sh"
-source "$BASE_PATH/andersonbosa/randoms.sh"
-source "$BASE_PATH/andersonbosa/termbin.sh"
-source "$BASE_PATH/andersonbosa/workrounds.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/ai_prompts.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/docker.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/generators.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/randoms.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/termbin.sh"
+source "$_MOSHEL_DIR_CUSTOM/andersonbosa/workrounds.sh"
 
-_moshell::log success "Loaded customizations."
+_moshell::log INFO "Loaded customizations."
