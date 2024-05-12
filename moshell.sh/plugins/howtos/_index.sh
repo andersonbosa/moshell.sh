68912452356a517baf31cc99659deb737e50ecba
andersonbosa <andersonbosa0@gmail.com>
2024-05-12 08:37:27 -0300

    chore rename main plugin file

--- moshell.sh/plugins/howtos/_index.sh
+++ moshell.sh/plugins/howtos/_index.sh
@@ -1,1 +1,11 @@
 # TODO: Plugin that teaches/shows how to do something
+
+ABSOLUTE_PATH_TO_THIS_FILE="${BASH_SOURCE:-$0}"
+ABSOLUTE_DIR_PATH_TO_THIS_FILE=$(dirname $ABSOLUTE_PATH_TO_THIS_FILE)
+
+load_plugin() {
+  source $ABSOLUTE_DIR_PATH_TO_THIS_FILE/plugin.sh
+}
+
+load_plugin
+
