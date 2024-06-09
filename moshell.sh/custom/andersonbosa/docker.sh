93253e3a4aa3757337850eb2ec8d0d181ab9a386
andersonbosa <andersonbosa0@gmail.com>
2024-06-09 10:32:54 -0300

    feat: introduce redis_up and redis_up_verbose functions

--- moshell.sh/custom/andersonbosa/docker.sh
+++ moshell.sh/custom/andersonbosa/docker.sh
@@ -1,18 +1,38 @@
 
-
-
-function docker_prune_none() {
+docker_prune_none() {
   for IMG in $(docker images | grep none | awk '{print $3}'); do
     docker image remove -f $IMG
   done
 }
 
-
-
-function docker_get_ip() {
+docker_get_ip() {
   local CONTAINER_ID="$1"
 
   docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID
 }
 
+redis_up() {
+  docker run --rm -p 6379:6379 --name redis -d redis:latest
+}
 
+redis_up_verbose() {
+  docker run --rm -p 6379:6379 --name redis redis:latest
+}
+
+docker_stop_all() {
+  for i in $( docker ps -q ); docker stop $i
+}
+
+_docker_logs_all_cleanup() {
+  kill $(jobs -p)
+  exit 0
+}
+
+docker_logs_all() {
+  trap _docker_logs_all_cleanup SIGINT SIGTERM
+  container_ids=$(docker ps -q)
+  for cid in $container_ids; do
+    docker logs -f $cid &
+  done
+  wait
+}
